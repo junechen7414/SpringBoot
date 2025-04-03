f6900ebc85ba5950b89f1222e5a62540180390da
Bobby Chen <BobbyChen@ibm.com>
2025-04-03 18:03:29 +0800

    Initial commit Exception Java

---
+++ Exception/ExceptionExample.java
@@ -0,0 +1,10 @@
+public class ExceptionExample {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0; // 可能發生 ArithmeticException
+        } catch (ArithmeticException e) {
+            System.out.println("錯誤: 除數不能為 0");
+        }
+        System.out.println("程式繼續執行...");
+    }
+}
