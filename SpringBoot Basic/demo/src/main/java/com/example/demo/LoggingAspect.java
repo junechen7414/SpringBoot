7b6735aeedbeac3ce061d60d0b30e9701a7bc5c4
Bobby Chen <BobbyChen@ibm.com>
2025-04-04 19:59:32 +0800

    Initial commit SpringBoot Basics

---
+++ SpringBoot Basic/demo/src/main/java/com/example/demo/LoggingAspect.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect  // 宣告此類別為切面
+@Component  // 讓 IoC 容器管理此切面元件
+public class LoggingAspect {
+
+    // 切面：在所有 根路徑 中的所有方法執行前進行日誌紀錄
+    @Before("execution(* com.example.demo.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("進入方法: " + joinPoint.getSignature().getName());
+    }
+}
