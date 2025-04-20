e0c43a4da537d31323e6e7eed387145a968b79b9
Bobby Chen <BobbyChen@ibm.com>
2025-04-20 08:40:06 +0800

    Remove setter in DTO which might never be used

--- SpringBoot Basic/demo/src/main/java/com/example/demo/Product_DTO.java
+++ SpringBoot Basic/demo/src/main/java/com/example/demo/Product_DTO.java
@@ -23,15 +23,7 @@ public class Product_DTO {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Integer getPrice() {
         return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
     }
 }
