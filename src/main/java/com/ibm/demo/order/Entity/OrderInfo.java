429aab7b13187270cdca1f9d69a48de985b0d217
Bobby <90739416+junechen7414@users.noreply.github.com>
2026-02-11 08:17:54 +0800

    OrderInfo entity sofe delete

--- src/main/java/com/ibm/demo/order/Entity/OrderInfo.java
+++ src/main/java/com/ibm/demo/order/Entity/OrderInfo.java
@@ -1,12 +1,18 @@
 package com.ibm.demo.order.Entity;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,14 +24,21 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Entity
+@SQLDelete(sql = "UPDATE ORDER_INFO SET DELETED = 1,DELETED_AT = CURRENT_TIMESTAMP,STATUS = 1003 WHERE ID = ?")
+@SQLRestriction("DELETED = 0") // 只選擇未刪除的資料
+@Builder
 @Table(name = "ORDER_INFO") // 指定對應的資料表名稱
 @Schema(description = "訂單資訊")
 public class OrderInfo {
@@ -57,6 +70,19 @@ public class OrderInfo {
     @ToString.Exclude //加在Entity中的OneToMany或ManyToOne的關聯上，否則會造成循環引用的問題，導致 StackOverflowError。
     private List<OrderDetail> orderDetails; // 存放該訂單下的所有產品明細
 
+    @Column(name = "DELETED", columnDefinition = "NUMBER(1)", nullable = false)
+    @Schema(description = "是否刪除", example = "false")
+    @Builder.Default
+    private Boolean deleted = false;
+
+    @Column(name = "DELETED_AT", columnDefinition = "TIMESTAMP")
+    @Schema(description = "刪除時間", example = "2024-06-01T12:00:00")
+    private LocalDateTime deletedAt;
+
+    public void restore() {
+        this.deleted = false;
+        this.deletedAt = null;
+    }
     @PrePersist
     public void prePersist() {
         this.createDate = LocalDate.now();
@@ -67,16 +93,4 @@ public class OrderInfo {
     public void preUpdate() {
         this.modifiedDate = LocalDate.now(); // 僅在更新時設置修改日期
     }
-
-    // helper
-    public void addOrderDetail(OrderDetail detail) {
-        orderDetails.add(detail);
-        detail.setOrderInfo(this); // 確保 OrderDetail 這邊的關聯也被設定
-    }
-
-    public void removeOrderDetail(OrderDetail detail) {
-        orderDetails.remove(detail);
-        detail.setOrderInfo(null); // 確保 OrderDetail 這邊的關聯被中斷
-    }
-
 }
