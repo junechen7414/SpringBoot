123f4b4cd72938e3b06bf81745c9658da9dba4a0
BobbyAtHome <ilovecrydad@gmail.com>
2026-02-20 12:37:47 +0800

    Entity欄位拆分後Repository跟著改欄位名

--- src/main/java/com/ibm/demo/product/ProductRepository.java
+++ src/main/java/com/ibm/demo/product/ProductRepository.java
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 import com.ibm.demo.product.DTO.GetProductListResponse;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    @Query("SELECT new com.ibm.demo.product.DTO.GetProductListResponse(p.id, p.name, p.price, p.saleStatus, p.stockQty) FROM Product p WHERE p.saleStatus = :saleStatus")
+    @Query("SELECT new com.ibm.demo.product.DTO.GetProductListResponse(p.id, p.name, p.price, p.saleStatus, p.available) FROM Product p WHERE p.saleStatus = :saleStatus")
     List<GetProductListResponse> findBySaleStatus(Integer saleStatus);
 
-    @Query("SELECT new com.ibm.demo.product.DTO.GetProductListResponse(p.id, p.name, p.price, p.saleStatus, p.stockQty) FROM Product p")
+    @Query("SELECT new com.ibm.demo.product.DTO.GetProductListResponse(p.id, p.name, p.price, p.saleStatus, p.available) FROM Product p")
     List<GetProductListResponse> findAllProducts();
 
     boolean existsByName(String name);
