2d8ac6d276e980b67d37db79d71bd486f36469e9
BobbyAtHome <ilovecrydad@gmail.com>
2026-02-22 15:45:46 +0800

    Configuration的欄位值建立獨立類別

---
+++ src/main/java/com/ibm/demo/AppProperties.java
@@ -0,0 +1,17 @@
+package com.ibm.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Validated
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+    @NotNull(message = "Base URL must not be null")
+    private String baseUrl;
+}
