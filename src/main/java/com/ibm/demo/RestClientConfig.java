71bb6121db629868e66a325d63fea8b5ecdb8b9d
Bobby <90739416+junechen7414@users.noreply.github.com>
2026-03-06 19:29:01 +0800

    加入底層client連線設定

--- src/main/java/com/ibm/demo/RestClientConfig.java
+++ src/main/java/com/ibm/demo/RestClientConfig.java
@@ -1,9 +1,17 @@
 package com.ibm.demo;
 
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -23,10 +31,32 @@ public class RestClientConfig {
         this.appProperties = appProperties;
     }
 
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(2000); // 總連線池上限
+        connectionManager.setDefaultMaxPerRoute(1500); // 每個單一服務 (如 Account) 的上限
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                // 關鍵：給予虛擬執行緒足夠的排隊時間，不要一拿不到連線就斷開
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectionRequestTimeout(Timeout.ofSeconds(5)) // 從池中拿連線可以等 5 秒
+                        .setResponseTimeout(Timeout.ofSeconds(10)) // 等待 API 回傳可等 10 秒
+                        .build())
+                .evictIdleConnections(TimeValue.ofSeconds(30))
+                .build();
+
+        factory.setHttpClient(httpClient);
+        return factory;
+    }
+
     @Bean
     public HttpServiceProxyFactory httpServiceProxyFactory(RestClient.Builder builder,
             RestClientErrorHandler errorHandler) {
         RestClient restClient = builder
+                .requestFactory(clientHttpRequestFactory()) // 顯式指定底層引擎
                 .baseUrl(appProperties.getBaseUrl())
                 .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> errorHandler.handle(response))
                 .build();
