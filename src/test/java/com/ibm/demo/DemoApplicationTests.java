d2bc950670ab46308e7e948e134c61a9fd4fe5c7
BobbyIBM <BobbyChen@ibm.com>
2026-03-24 17:13:55 +0800

    rename JUnit test tag

--- src/test/java/com/ibm/demo/DemoApplicationTests.java
+++ src/test/java/com/ibm/demo/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Tag("SpringBoot SanityTest")
+@Tag("SanityTest")
 @SpringBootTest
 class DemoApplicationTests {
 
