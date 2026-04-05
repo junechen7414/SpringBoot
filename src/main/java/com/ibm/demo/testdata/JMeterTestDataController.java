2b416a92776a635866e9aeb45273135b33e9bb27
BobbyAtHome <ilovecrydad@gmail.com>
2026-04-05 14:08:59 +0800

    依照環境變數分Profile，每個Bean可有Profile，依Profile拆分Controller

---
+++ src/main/java/com/ibm/demo/testdata/JMeterTestDataController.java
@@ -0,0 +1,25 @@
+package com.ibm.demo.testdata;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/JMeterTestData")
+@RequiredArgsConstructor
+@Profile("dev")
+public class JMeterTestDataController {
+    private final TestDataService testDataService;
+
+    @PostMapping("/createJMeterPrecondition")
+    public ResponseEntity<String> createJMeterPrecondition(@RequestParam Integer count) {
+        testDataService.createOrderPrecondition(count);
+        return ResponseEntity.ok(count + "Account and Product record created successfully.");
+    }
+
+}
